a09824ce6d3c0d7a3f3dae8035de38a8e2ee7ca4
Shubham <shubham@gmail.com>
2024-09-20 16:26:21 +0530

    Add matches_played_per_team_each_season function

---
+++ src/server/high-order-functions/1-matches-per-year.js
@@ -0,0 +1,22 @@
+import matches from '../../data/matches.json' assert { type: 'json'};
+import fs from 'fs';
+
+
+function matches_played_per_team_each_season() {
+    const played_teams = matches.reduce((acc, {  season }) => {
+        // const season = match['season'];
+        if (!acc[season]) {
+            acc[season] = 1;
+        }else{
+            acc[season] += 1; 
+        }
+        return acc;
+    },{});
+    return played_teams;
+}
+
+// console.log(matches_played_per_team_each_season());
+
+const count_for_matches_per_season = matches_played_per_team_each_season();
+
+fs.writeFileSync('/home/shubham/Desktop/Projetc/IPL/src/public/output_02/1_matches_per_year.json', JSON.stringify(count_for_matches_per_season, null, 2));
