7168ac4f6fb6e1fd67f3c63449620a54abbc2b7b
Shubham <shubham@gmail.com>
2024-09-20 10:31:45 +0530

    Produce output for 3_extra_runs_conceded_per_team_year_2016

--- src/server/3-extra-runs-conceded-per-team-year-2016.js
+++ src/server/3-extra-runs-conceded-per-team-year-2016.js
@@ -1,5 +1,5 @@
 import deliveries from '../data/deliveries.json' assert { type: 'json'};
-
+import fs from 'fs';
 import matches from '../data/matches.json' assert { type: 'json'};
 
 
@@ -8,31 +8,31 @@ import matches from '../data/matches.json' assert { type: 'json'};
 // console.log(matches[0].season);
 
 
-function get_2016_MatchID() {
-    let matchIdx = [];
+function get_2016_Match_ID() {
+    let match_Idx_2016 = [];
 
     for (let index = 0; index < matches.length; index++) {
         if (matches[index].season == 2016){
-            matchIdx.push(matches[index].id);
+            match_Idx_2016.push(matches[index].id);
         }
         
     }
-    return matchIdx;
+    return match_Idx_2016;
 }
 
-// console.log(getMatchID());
+// console.log(get_2016_Match_ID());
 
-function extrasConcededPerTeamYear_2016(params) {
+function extras_conceded_per_team_per_year_2016(params) {
     let extras_2016 = {}
 
-    const matcheIDX = new Set(get_2016_MatchID());
+    const match_IDX = new Set(get_2016_Match_ID());
 
     
     for (let index = 0; index < deliveries.length; index++) {
         const { match_id, bowling_team, extra_runs } = deliveries[index];
         // console.log(`id: ${match_id}, bowling: ${bowling_team} and extras: ${extra_runs}`);
         
-        if (matcheIDX.has(match_id)) {
+        if (match_IDX.has(match_id)) {
             if (extras_2016[bowling_team]) {
                 extras_2016[bowling_team] += Number.parseInt(extra_runs);
             } else {
@@ -43,4 +43,9 @@ function extrasConcededPerTeamYear_2016(params) {
     return extras_2016;
 }
 
-console.log(extrasConcededPerTeamYear_2016());
+// console.log(extras_conceded_per_team_per_year_2016());
+
+const extras_2016_conceded_per_team = extras_conceded_per_team_per_year_2016();
+
+
+fs.writeFileSync('/home/shubham/Desktop/Projetc/IPL/src/public/output/3_extra_runs_conceded_per_team_year_2016.json', JSON.stringify(extras_2016_conceded_per_team, null, 2));
