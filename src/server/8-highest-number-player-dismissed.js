622eaaffaca7918497dcc22968dcc4cfc03e5a9e
Shubham <shubham@gmail.com>
2024-09-20 11:04:26 +0530

    Produce output for 8-highest-number-player-dismissed

--- src/server/8-highest-number-player-dismissed.js
+++ src/server/8-highest-number-player-dismissed.js
@@ -1,54 +1,62 @@
 import deliveries from '../data/deliveries.json' assert { type: 'json'};
+import fs from 'fs';
 
-function playerDismissed() {
-    let playerDismissedObj = {};
+
+
+function player_dismissed() {
+    let player_dismissed_obj = {};
 
     for (let index = 0; index < deliveries.length; index++) {
         const { player_dismissed, batsman, bowler } = deliveries[index];
         if (player_dismissed !== "") {
-            if (!playerDismissedObj[batsman]) {
-                playerDismissedObj[batsman] = {};
-                playerDismissedObj[batsman][bowler] = 1;
+            if (!player_dismissed_obj[batsman]) {
+                player_dismissed_obj[batsman] = {};
+                player_dismissed_obj[batsman][bowler] = 1;
 
             } else {
-                if (!playerDismissedObj[batsman][bowler]) {
-                    playerDismissedObj[batsman][bowler] = 1;
+                if (!player_dismissed_obj[batsman][bowler]) {
+                    player_dismissed_obj[batsman][bowler] = 1;
                 } else {
-                    playerDismissedObj[batsman][bowler] += 1;
+                    player_dismissed_obj[batsman][bowler] += 1;
                 }
             }
         }
 
     }
 
-    return playerDismissedObj;
+    return player_dismissed_obj;
 }
 
-// console.log(playerDismissed());
+// console.log(player_dismissed());
 
-function maxTimesPlayerDismissed() {
-    let playerObj = playerDismissed();
+function max_times_player_dismissed() {
+    let player_obj = player_dismissed();
     // console.log(playerObj);
-    let dismissedPlayer = {};
+    let dismissed_player = {};
     let max = 0;
-    let bowlerName = "";
-    let batsmanName = "";
-    for (let batsman in playerObj) {
-        let obj = playerObj[batsman];
+    let bowler_name = "";
+    let batsman_name = "";
+    for (let batsman in player_obj) {
+        let obj = player_obj[batsman];
         for (let bowler in obj) {
             if (max < obj[bowler]) {
                 max = obj[bowler];
-                batsmanName = batsman;
-                bowlerName = bowler;
+                batsman_name = batsman;
+                bowler_name = bowler;
             }
         }
     }
-    dismissedPlayer[batsmanName] = {};
-    dismissedPlayer[batsmanName][bowlerName] = max;
-    return dismissedPlayer;
+    dismissed_player[batsman_name] = {};
+    dismissed_player[batsman_name][bowler_name] = max;
+    return dismissed_player;
 
 
 }
 
 
-console.log(maxTimesPlayerDismissed());
+// console.log(max_times_player_dismissed());
+
+const player_out_max_times = max_times_player_dismissed();
+
+
+fs.writeFileSync('/home/shubham/Desktop/Projetc/IPL/src/public/output/8_highest_number_player_dismissed.json', JSON.stringify(player_out_max_times, null, 2));
