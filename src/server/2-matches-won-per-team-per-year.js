abb64d43f64924a37ad7b00ccc852d6c0cb62fa2
Shubham <shubham@gmail.com>
2024-09-20 10:23:27 +0530

    Produce output for 2-matches-won-per-team-per-year

--- src/server/2-matches-won-per-team-per-year.js
+++ src/server/2-matches-won-per-team-per-year.js
@@ -1,8 +1,8 @@
 import fs from 'fs';
 import matches from '../data/matches.json' assert { type: 'json'};
 
-function matchesWonPerTeamYear() {
-    let winnerCount = {};
+function matches_won_per_team_per_year() {
+    let winner_Count = {};
 
     for (let i = 0; i < matches.length; i++) {
         const seasons = matches[i]['season'];
@@ -10,22 +10,26 @@ function matchesWonPerTeamYear() {
 
         
         
-        if (seasons in winnerCount) {
-            if (winnerCount[seasons][winners]) {
-                winnerCount[seasons][winners] += 1;
+        if (seasons in winner_Count) {
+            if (winner_Count[seasons][winners]) {
+                winner_Count[seasons][winners] += 1;
             }else{
                 if (winners === '') {
                     continue;
                 }
-                winnerCount[seasons][winners] = 1;
+                winner_Count[seasons][winners] = 1;
             }
             
         }else{
-            winnerCount[seasons] = {};
+            winner_Count[seasons] = {};
         }
     }
-    console.log(winnerCount);
+    // console.log(winner_Count);
+    return winner_Count;
 }
-matchesWonPerTeamYear();
+// matchesWonPerTeamYear();
+
+const per_year_winning_count_team = matches_won_per_team_per_year();
 
 
+fs.writeFileSync('/home/shubham/Desktop/Projetc/IPL/src/public/output/2_matches_won_per_team_per_year.json', JSON.stringify(per_year_winning_count_team, null, 2));
