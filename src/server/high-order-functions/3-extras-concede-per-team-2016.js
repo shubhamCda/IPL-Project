c8dd908b02c6f53df60731d8c705e1bb343be704
Shubham <shubham@gmail.com>
2024-09-21 12:49:36 +0530

    Add extras_concede_2016 function

---
+++ src/server/high-order-functions/3-extras-concede-per-team-2016.js
@@ -0,0 +1,37 @@
+import deliveries from '../../data/deliveries.json' assert { type: 'json'};
+import fs from 'fs';
+import matches from '../../data/matches.json' assert { type: 'json'};
+
+
+
+
+
+const get_2016_match_idx = matches.reduce((acc, { id, season }) => {
+    if (season === '2016') {
+        acc.push(id);
+    }
+    return acc;
+}, []);
+
+// console.log(get_2016_match_idx);
+const idx_2016 = new Set(get_2016_match_idx);
+
+const extras_conceded_2016 = deliveries.reduce((acc, { bowling_team, match_id, extra_runs }) => {
+    if (idx_2016.has(match_id)) {
+        if (extra_runs !== '0') {
+
+            if (!acc[bowling_team]) {
+                acc[bowling_team] = parseInt(extra_runs)
+            } else {
+                acc[bowling_team] += parseInt(extra_runs);
+            }
+        }
+
+    }
+    return acc;
+}, {});
+
+// console.log(extras_conceded_2016);
+
+
+fs.writeFileSync('/home/shubham/Desktop/Projetc/IPL/src/public/output_02/03_extras_conceded_per_team_2016.json', JSON.stringify(extras_conceded_2016, null, 2));
