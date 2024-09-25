432aaa7b2aa3cb6395ab017613a0510379c802ba
Shubham <shubham@gmail.com>
2024-09-25 12:06:33 +0530

    Add comments to 7-strike-rate-of-batsman-per-season.js file

--- src/server/7-strike-rate-of-batsman-per-season.js
+++ src/server/7-strike-rate-of-batsman-per-season.js
@@ -1,93 +1,104 @@
 import deliveries from '../data/deliveries.json' assert { type: 'json'};
-
 import matches from '../data/matches.json' assert { type: 'json'};
 
-import fs from 'fs';
 
 
+//to get match id's with respect to all the seasons
 function get_matches_idx() {
     let match_idx = {};
 
-    for (let index = 0; index < matches.length; index++) {
-        const { id, season } = matches[index];
+    try {
+        
+        for (let index = 0; index < matches.length; index++) {
+            const { id, season } = matches[index];
 
-        match_idx[id] = season;
+            match_idx[id] = season;
+        }
+        return match_idx;
+    } catch (error) {
+        console.error("Error while processing the match id's with respect to all the seasons : ", error);
+        return {};
     }
-    return match_idx;
+
 }
 
-// console.log(get_matches_idx());
-
+// to get the data of total runs and balls of the batsman
 function player_info() {
     let match_idx = get_matches_idx();
-    // console.log(match_idx);
+
     let batsman_data = {};
 
-    for (let index = 0; index < deliveries.length; index++) {
-        const { match_id, batsman, total_runs } = deliveries[index];
-
-        let season = match_idx[match_id];
-        // console.log(season);
-
-        if (batsman_data[season]) {
-            if (batsman_data[season][batsman]) {
-                batsman_data[season][batsman]['totalRuns'] += Number(total_runs);
-                batsman_data[season][batsman]['balls'] += 1;
+    try {
+        for (let index = 0; index < deliveries.length; index++) {
+            const { match_id, batsman, total_runs } = deliveries[index];
+    
+            let season = match_idx[match_id];
+           
+    
+            if (batsman_data[season]) {
+                if (batsman_data[season][batsman]) {
+                    batsman_data[season][batsman]['totalRuns'] += Number(total_runs);
+                    batsman_data[season][batsman]['balls'] += 1;
+                } else {
+                    batsman_data[season][batsman] = {
+                        totalRuns: 0,
+                        balls: 0
+                    }
+                    batsman_data[season][batsman]['totalRuns'] += Number(total_runs);
+                    batsman_data[season][batsman]['balls'] += 1;
+                }
             } else {
+                batsman_data[season] = {};
                 batsman_data[season][batsman] = {
                     totalRuns: 0,
                     balls: 0
                 }
                 batsman_data[season][batsman]['totalRuns'] += Number(total_runs);
                 batsman_data[season][batsman]['balls'] += 1;
+    
             }
-        } else {
-            batsman_data[season] = {};
-            batsman_data[season][batsman] = {
-                totalRuns: 0,
-                balls: 0
-            }
-            batsman_data[season][batsman]['totalRuns'] += Number(total_runs);
-            batsman_data[season][batsman]['balls'] += 1;
-
         }
+    
+        return batsman_data;
+    } catch (error) {
+        console.error("Error while processing the data of total runs and balls of the batsman : ", error);
+        return {};
     }
-    // console.log(batsman_data);
-    return batsman_data;
     
 }
 
-// player_info();
 
-function batsman_strike_rate() {
+// to get the strike rate of a batsman for each season
+export function batsman_strike_rate() {
     let player_data = player_info();
-    // console.log(player_data);
+   
     let batsman_strike_rate = {}
 
-    for(let season in player_data) {
-        // console.log(player_data[season]);
-        let data = player_data[season];
-        batsman_strike_rate[season] = {};
-        for (let player in data){
-            const { totalRuns, balls } = data[player];
-
-            let player_strike_rate = (totalRuns/balls) * 100;
-
-            batsman_strike_rate[season][player] = player_strike_rate.toFixed(2);
-
-            // console.log(player_strike_rate);
-            
-
+    try {
+        for(let season in player_data) {
+  
+            let data = player_data[season];
+            batsman_strike_rate[season] = {};
+            for (let player in data){
+                const { totalRuns, balls } = data[player];
+    
+                let player_strike_rate = (totalRuns/balls) * 100;
+    
+                batsman_strike_rate[season][player] = player_strike_rate.toFixed(2);
+    
+        
+                
+    
+            }
         }
+      
+        return batsman_strike_rate;
+    } catch (error) {
+        console.error("Error while processing the strike rate of a batsman for each season : ", error);
+        return {};
     }
-    // console.log(batsman_strike_rate);
-    return batsman_strike_rate;
     
 }
 
-// console.log(batsman_strike_rate());
 
-const strike_rate = batsman_strike_rate();
- 
 
-fs.writeFileSync('/home/shubham/Desktop/Projetc/IPL/src/public/output/7_strike_rate_of_batsman_per_season.json', JSON.stringify(strike_rate, null, 2));
