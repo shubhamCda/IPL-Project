e83255e496d73040f44d21f90a7c466ea65eca65
Shubham <shubham@gmail.com>
2024-09-25 12:19:17 +0530

    Add try-catch block in 8-highest-number-player-dismissed.js

--- src/server/8-highest-number-player-dismissed.js
+++ src/server/8-highest-number-player-dismissed.js
@@ -1,62 +1,69 @@
 import deliveries from '../data/deliveries.json' assert { type: 'json'};
-import fs from 'fs';
 
 
 
+// to get the count of player dismissed by the same bowler
 function player_dismissed() {
     let player_dismissed_obj = {};
 
-    for (let index = 0; index < deliveries.length; index++) {
-        const { player_dismissed, batsman, bowler } = deliveries[index];
-        if (player_dismissed !== "") {
-            if (!player_dismissed_obj[batsman]) {
-                player_dismissed_obj[batsman] = {};
-                player_dismissed_obj[batsman][bowler] = 1;
-
-            } else {
-                if (!player_dismissed_obj[batsman][bowler]) {
+    try {
+        for (let index = 0; index < deliveries.length; index++) {
+            const { player_dismissed, batsman, bowler } = deliveries[index];
+            if (player_dismissed !== "") {
+                if (!player_dismissed_obj[batsman]) {
+                    player_dismissed_obj[batsman] = {};
                     player_dismissed_obj[batsman][bowler] = 1;
+    
                 } else {
-                    player_dismissed_obj[batsman][bowler] += 1;
+                    if (!player_dismissed_obj[batsman][bowler]) {
+                        player_dismissed_obj[batsman][bowler] = 1;
+                    } else {
+                        player_dismissed_obj[batsman][bowler] += 1;
+                    }
                 }
             }
+    
         }
-
+    
+        return player_dismissed_obj;
+    } catch (error) {
+        console.error("Error while processing the count of player dismissed by the same bowler : ", error);
+        return {};
     }
-
-    return player_dismissed_obj;
 }
 
-// console.log(player_dismissed());
 
-function max_times_player_dismissed() {
+// to get the highest number of times one player has been dismissed by another player
+export function max_times_player_dismissed() {
     let player_obj = player_dismissed();
-    // console.log(playerObj);
+
     let dismissed_player = {};
     let max = 0;
     let bowler_name = "";
     let batsman_name = "";
-    for (let batsman in player_obj) {
-        let obj = player_obj[batsman];
-        for (let bowler in obj) {
-            if (max < obj[bowler]) {
-                max = obj[bowler];
-                batsman_name = batsman;
-                bowler_name = bowler;
+
+   try {
+        for (let batsman in player_obj) {
+            let obj = player_obj[batsman];
+            for (let bowler in obj) {
+                if (max < obj[bowler]) {
+                    max = obj[bowler];
+                    batsman_name = batsman;
+                    bowler_name = bowler;
+                }
             }
         }
-    }
-    dismissed_player[batsman_name] = {};
-    dismissed_player[batsman_name][bowler_name] = max;
-    return dismissed_player;
+        dismissed_player[batsman_name] = {};
+        dismissed_player[batsman_name][bowler_name] = max;
+        return dismissed_player;
+   } catch (error) {
+    console.error("Error while processing tthe highest number of times one player has been dismissed by another player: ", error);
+    return {};
+   }
 
 
 }
 
 
-// console.log(max_times_player_dismissed());
-
-const player_out_max_times = max_times_player_dismissed();
 
 
-fs.writeFileSync('/home/shubham/Desktop/Projetc/IPL/src/public/output/8_highest_number_player_dismissed.json', JSON.stringify(player_out_max_times, null, 2));
