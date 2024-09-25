626c0b161be8ee944f6710f4f88fe5994da19170
Shubham <shubham@gmail.com>
2024-09-25 10:02:06 +0530

    Add try-catch block in matches_won_per_team_per_year function

--- src/server/2-matches-won-per-team-per-year.js
+++ src/server/2-matches-won-per-team-per-year.js
@@ -1,36 +1,38 @@
-import fs from 'fs';
 import matches from '../data/matches.json' assert { type: 'json'};
 
-function matches_won_per_team_per_year() {
-    let winner_Count = {};
+// Number of matches won per team per year in IPL.
+export function matches_won_per_team_per_year() {
+    let winner_count = {};
+    try{
 
-    for (let i = 0; i < matches.length; i++) {
-        const seasons = matches[i]['season'];
-        const winners = matches[i]['winner'];
+        for (let i = 0; i < matches.length; i++) {
+            const seasons = matches[i]['season'];
+            const winners = matches[i]['winner'];
 
-        
-        
-        if (seasons in winner_Count) {
-            if (winner_Count[seasons][winners]) {
-                winner_Count[seasons][winners] += 1;
-            }else{
-                if (winners === '') {
-                    continue;
-                }
-                winner_Count[seasons][winners] = 1;
-            }
             
-        }else{
-            winner_Count[seasons] = {};
-            winner_Count[seasons][winners] = 1;
+            
+            if (seasons in winner_count) {
+                if (winner_count[seasons][winners]) {
+                    winner_count[seasons][winners] += 1;
+                }else{
+                    if (winners === '') {
+                        continue;
+                    }
+                    winner_count[seasons][winners] = 1;
+                }
+                
+            }else{
+                winner_count[seasons] = {};
+                winner_count[seasons][winners] = 1;
+            }
         }
+        
+        return winner_count;
     }
-    // console.log(winner_Count);
-    return winner_Count;
+    catch (error){
+        console.error("Error while processing matches per year data: ", error);
+        return {};
+    }
 }
-// matchesWonPerTeamYear();
-
-const per_year_winning_count_team = matches_won_per_team_per_year();
 
 
-fs.writeFileSync('/home/shubham/Desktop/Projetc/IPL/src/public/output/2_matches_won_per_team_per_year.json', JSON.stringify(per_year_winning_count_team, null, 2));
