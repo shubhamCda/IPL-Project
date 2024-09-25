7118e4a0de85ffc5d6006ad63c3cf43c6447e188
Shubham <shubham@gmail.com>
2024-09-25 11:31:03 +0530

    Add try-catch block in match_and_toss_winner function

--- src/server/5-toss-and-match-won.js
+++ src/server/5-toss-and-match-won.js
@@ -1,32 +1,31 @@
-import deliveries from '../data/deliveries.json' assert {type: 'json'};
 import matches from '../data/matches.json' assert {type: 'json'};
-import fs from 'fs';
 
 
-function match_and_toss_winner() {
-    let result_obj = {};
+// the number of times each team won the toss and also won the match
+export function match_and_toss_winner() {
+    let winners = {};
 
-    for (let index = 0; index < matches.length; index++) {
+    try {
+        for (let index = 0; index < matches.length; index++) {
         
-        const { toss_winner, winner} = matches[index];
-
-        if (toss_winner === winner) {
-            if (result_obj[toss_winner]) {
-                
-                result_obj[toss_winner] += 1;
-            }else{
-                result_obj[toss_winner] = 1;
-            }
-        }      
-        
+            const { toss_winner, winner} = matches[index];
+    
+            if (toss_winner === winner) {
+                if (winners[toss_winner]) {
+                    
+                    winners[toss_winner] += 1;
+                }else{
+                    winners[toss_winner] = 1;
+                }
+            }      
+            
+        }
+        return winners;
+    } catch (error) {
+        console.error("Error while processing the number of times each team won the toss and also won the match : ", error);
+        return {};
     }
-    return result_obj;
 }
 
-// console.log(match_and_toss_winner());
-
-const winners = match_and_toss_winner();
 
 
-
-fs.writeFileSync('/home/shubham/Desktop/Projetc/IPL/src/public/output/5_toss_and_match_won.json', JSON.stringify(winners, null, 2));
--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,7 @@ import { matches_per_year } from './1-matches-per-year.js'
 import { matches_won_per_team_per_year } from './2-matches-won-per-team-per-year.js';
 import { extras_conceded_per_team_per_year_2016 } from './3-extra-runs-conceded-per-team-year-2016.js';
 import { top_10_economical_bowlers } from './4-top-10-economical-bowlers-2015.js';
+import { match_and_toss_winner } from './5-toss-and-match-won.js';
 
 //1. To calculate number of matches played per year for all the years in IPL.
 const matches_per_season_count = matches_per_year();
@@ -27,4 +28,10 @@ const extras_2016_conceded_per_team = extras_conceded_per_team_per_year_2016();
 //4. Top 10 economical bowlers in the year 2015
 const top_economical_bowlers = top_10_economical_bowlers();
 
-fs.writeFileSync('/home/shubham/Desktop/Projetc/IPL/src/public/output/4-top-10-economical-bowlers-2015.json', JSON.stringify(top_economical_bowlers, null, 2));
+// fs.writeFileSync('/home/shubham/Desktop/Projetc/IPL/src/public/output/4-top-10-economical-bowlers-2015.json', JSON.stringify(top_economical_bowlers, null, 2));
+
+
+// Find the number of times each team won the toss and also won the match
+const winners = match_and_toss_winner();
+
+fs.writeFileSync('/home/shubham/Desktop/Projetc/IPL/src/public/output/5_toss_and_match_won.json', JSON.stringify(winners, null, 2));
