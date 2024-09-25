e6457e7d734320c1fc92f10397a1f40dd220582d
Shubham <shubham@gmail.com>
2024-09-25 10:39:49 +0530

    Refactor extras_conceded_per_team_per_year_2016 function

--- src/server/3-extra-runs-conceded-per-team-year-2016.js
+++ src/server/3-extra-runs-conceded-per-team-year-2016.js
@@ -1,38 +1,38 @@
 import deliveries from '../data/deliveries.json' assert { type: 'json'};
-import fs from 'fs';
 import matches from '../data/matches.json' assert { type: 'json'};
 
 
+//To get the match ID's from season 2016 
+function get_2016_match_id() {
+    let match_idx_2016 = [];
 
-// console.log(deliveries['match_id']);
-// console.log(matches[0].season);
-
-
-function get_2016_Match_ID() {
-    let match_Idx_2016 = [];
-
-    for (let index = 0; index < matches.length; index++) {
-        if (matches[index].season == 2016){
-            match_Idx_2016.push(matches[index].id);
+    try {
+        for (let index = 0; index < matches.length; index++) {
+            if (matches[index].season == 2016){
+                match_idx_2016.push(matches[index].id);
+            }
+            
         }
-        
+        return match_idx_2016;
+    } catch (error) {
+        console.error("Error while processing match ID's from season 2016 : ", error);
+        return [];
     }
-    return match_Idx_2016;
 }
 
-// console.log(get_2016_Match_ID());
 
-function extras_conceded_per_team_per_year_2016() {
+// Extra runs conceded per team in the year 2016
+export function extras_conceded_per_team_per_year_2016() {
     let extras_2016 = {}
 
-    const match_IDX = new Set(get_2016_Match_ID());
+    try {
+        const match_idx = new Set(get_2016_match_id());
 
     
     for (let index = 0; index < deliveries.length; index++) {
         const { match_id, bowling_team, extra_runs } = deliveries[index];
-        // console.log(`id: ${match_id}, bowling: ${bowling_team} and extras: ${extra_runs}`);
-        
-        if (match_IDX.has(match_id)) {
+                
+        if (match_idx.has(match_id)) {
             if (extras_2016[bowling_team]) {
                 extras_2016[bowling_team] += Number.parseInt(extra_runs);
             } else {
@@ -41,11 +41,8 @@ function extras_conceded_per_team_per_year_2016() {
         }         
     }
     return extras_2016;
+    } catch (error) {
+        console.error("Error while processing Extra runs conceded per team in the year 2016 : ", error);
+        return {};
+    }
 }
-
-// console.log(extras_conceded_per_team_per_year_2016());
-
-const extras_2016_conceded_per_team = extras_conceded_per_team_per_year_2016();
-
-
-fs.writeFileSync('/home/shubham/Desktop/Projetc/IPL/src/public/output/3_extra_runs_conceded_per_team_year_2016.json', JSON.stringify(extras_2016_conceded_per_team, null, 2));
