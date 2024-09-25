c28429b18ecadbfa9393033125b7a5c9cde1268b
Shubham <shubham@gmail.com>
2024-09-25 11:15:40 +0530

    Add try-catch block in 4-top-10-economical-bowlers-2015. js functions

--- src/server/4-top-10-economical-bowlers-2015.js
+++ src/server/4-top-10-economical-bowlers-2015.js
@@ -2,96 +2,113 @@ import deliveries from '../data/deliveries.json' assert { type: 'json'};
 import fs from 'fs';
 import matches from '../data/matches.json' assert { type: 'json'};
 
-function get_2015_Match_Idx() {
-    let matchIdx_2015 = [];
 
-    for (let index = 0; index < matches.length; index++) {
-        const { id, season } = matches[index];
+//get match id's for season 2015
+function get_2015_match_idx() {
+    let match_idx_2015 = [];
 
-        if (season == 2015) {
-
-            matchIdx_2015.push(id);
-        }
-
-    }
-    return matchIdx_2015;
-}
-
-// console.log(get_2015_Match_Idx());
-
-
-function all_economical_bowlers() {
-    let econnomical_bowlers = {};
-
-
-    const match_idx = get_2015_Match_Idx()
-    const matchIdx_set = new Set(match_idx);
-
-    for (let index = 0; index < deliveries.length; index++) {
-        const { total_runs, bowler, match_id, wide_runs, noball_runs } = deliveries[index];
-
-        if (matchIdx_set.has(match_id)) {
-
-            if (econnomical_bowlers[bowler]) {
-                econnomical_bowlers[bowler]['Total_Runs'] += Number.parseInt(total_runs);
-
-                if (noball_runs === '0' && wide_runs === '0') {
-
-                    econnomical_bowlers[bowler]['Balls'] += 1;
-                }
-
-
-            } else {
-                econnomical_bowlers[bowler] = {};
-                econnomical_bowlers[bowler] = {
-                    Total_Runs: 0,
-                    Balls: 0
-                }
+    try {
+        for (let index = 0; index < matches.length; index++) {
+            const { id, season } = matches[index];
+    
+            if (season == 2015) {
+    
+                match_idx_2015.push(id);
             }
-
+    
         }
-
+        return match_idx_2015;
+    } catch (error) {
+        console.error("Error while processing match id's for season 2015 : ", error);
+        return [];
     }
-    return econnomical_bowlers;
-
 }
-// console.log(all_economical_bowlers());
 
-function top_10_economical_bowlers() {
+
+
+//get the runs and balls of all the bowlers from season 2015
+function all_economical_bowlers() {
+    let all_bowlers = {};
+
+
+    const match_idx = get_2015_match_idx()
+    const match_idx_set = new Set(match_idx);
+
+    try {
+        for (let index = 0; index < deliveries.length; index++) {
+            const { total_runs, bowler, match_id, wide_runs, noball_runs } = deliveries[index];
+    
+            if (match_idx_set.has(match_id)) {
+    
+                if (all_bowlers[bowler]) {
+                    all_bowlers[bowler]['Total_Runs'] += Number.parseInt(total_runs);
+    
+                    if (noball_runs === '0' && wide_runs === '0') {
+    
+                        all_bowlers[bowler]['Balls'] += 1;
+                    }
+    
+    
+                } else {
+                    all_bowlers[bowler] = {};
+                    all_bowlers[bowler] = {
+                        Total_Runs: 0,
+                        Balls: 0
+                    }
+                }
+    
+            }
+    
+        }
+        return all_bowlers;
+    
+    } catch (error) {
+        console.error("Error while processing runs and balls of all the bowlers from season 2015 : ", error);
+        return {};
+    }
+}
+
+// Economy rate of top 10 economical bowlers in the year 2015
+export function top_10_economical_bowlers() {
     let economical_bowlers = all_economical_bowlers();
-    // console.log(economicalBowlers);
+    
     let bowlers_keys = Object.keys(economical_bowlers)
-    // console.log(bowlersKeys);
+    
 
     let economy_rate = [];
     let result = [];
 
-    for (let index = 0; index < bowlers_keys.length; index++) {
-        // console.log(bowlersKeys[index]);
-        // console.log(economicalBowlers[bowlersKeys[index]]);
-
-        const { Total_Runs, Balls } = economical_bowlers[bowlers_keys[index]];
-
-        // console.log(`TotalRuns: ${Total_Runs} and Ball: ${Balls}`);
-
-        const economy = (Total_Runs / (Balls / 6)).toFixed(2);
-        // console.log(economy);
-
-
-        economy_rate.push({ bowler: bowlers_keys[index], economy: parseFloat(economy) })
-
-
+    try {
+        for (let index = 0; index < bowlers_keys.length; index++) {
+            // console.log(bowlersKeys[index]);
+            // console.log(economicalBowlers[bowlersKeys[index]]);
+    
+            const { Total_Runs, Balls } = economical_bowlers[bowlers_keys[index]];
+    
+            // console.log(`TotalRuns: ${Total_Runs} and Ball: ${Balls}`);
+    
+            const economy = (Total_Runs / (Balls / 6)).toFixed(2);
+            // console.log(economy);
+    
+    
+            economy_rate.push({ bowler: bowlers_keys[index], economy: parseFloat(economy) })
+    
+    
+        }
+        economy_rate.sort((a, b) => a.economy - b.economy);
+        // console.log(economyRate);
+    
+        result.push(economy_rate.slice(0, 10));
+    
+        return result;
+    } catch (error) {
+        console.error("Error while processing top 10 economical bowlers in the year 2015 : ", error);
+        return {};
     }
-    economy_rate.sort((a, b) => a.economy - b.economy);
-    // console.log(economyRate);
 
-    result.push(economy_rate.slice(0, 10));
-
-    return result;
+    
 
 }
-// console.log(top_10_economical_bowlers());
 
-const top_economical_bowlers = top_10_economical_bowlers();
 
-fs.writeFileSync('/home/shubham/Desktop/Projetc/IPL/src/public/output/4_top_10_economical_bowlers_2015.json', JSON.stringify(top_economical_bowlers, null, 2));
+
