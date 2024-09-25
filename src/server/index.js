531a440149307b4d34aa717172a80207df6f76e1
Shubham <shubham@gmail.com>
2024-09-25 11:16:53 +0530

    Add top_10_economical_bowlers function

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,7 @@ import fs from 'fs';
 import { matches_per_year } from './1-matches-per-year.js'
 import { matches_won_per_team_per_year } from './2-matches-won-per-team-per-year.js';
 import { extras_conceded_per_team_per_year_2016 } from './3-extra-runs-conceded-per-team-year-2016.js';
+import { top_10_economical_bowlers } from './4-top-10-economical-bowlers-2015.js';
 
 //1. To calculate number of matches played per year for all the years in IPL.
 const matches_per_season_count = matches_per_year();
@@ -20,4 +21,10 @@ const per_year_winning_count_team = matches_won_per_team_per_year();
 //3. Extra runs conceded per team in the year 2016
 const extras_2016_conceded_per_team = extras_conceded_per_team_per_year_2016();
 
-fs.writeFileSync('/home/shubham/Desktop/Projetc/IPL/src/public/output/3-extra-runs-conceded-per-team-year-2016.json', JSON.stringify(extras_2016_conceded_per_team, null, 2));
+// fs.writeFileSync('/home/shubham/Desktop/Projetc/IPL/src/public/output/3-extra-runs-conceded-per-team-year-2016.json', JSON.stringify(extras_2016_conceded_per_team, null, 2));
+
+
+//4. Top 10 economical bowlers in the year 2015
+const top_economical_bowlers = top_10_economical_bowlers();
+
+fs.writeFileSync('/home/shubham/Desktop/Projetc/IPL/src/public/output/4-top-10-economical-bowlers-2015.json', JSON.stringify(top_economical_bowlers, null, 2));
