f834f700c4d3fdc9b2962d2539d7d7b0bbb4b5e0
Shubham <shubham@gmail.com>
2024-09-25 09:40:56 +0530

    Add try-catch block in matches_per_year function

--- src/server/1-matches-per-year.js
+++ src/server/1-matches-per-year.js
@@ -4,7 +4,7 @@ import matches from '../data/matches.json' assert { type: 'json'};
 
 //To calculate number of matches played per year for all the years in IPL.
 
-function matches_per_year() {
+export function matches_per_year() {
     let matches_count = {};
 
     for (let i = 0; i < matches.length; i++) {
@@ -21,7 +21,6 @@ function matches_per_year() {
     return matches_count;
 }
 
-const matches_per_season_count = matches_per_year();
 
 
-fs.writeFileSync('/home/shubham/Desktop/Projetc/IPL/src/public/output/1_matches_per_year.json', JSON.stringify(matches_per_season_count, null, 2));
+
